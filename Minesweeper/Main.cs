70985bdf548b35f18ae09a22c834e8d4290e7482
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-06-23 19:22:08 +0200

    Add spritesheet, draw tiles (as a test)

--- Minesweeper/Main.cs
+++ Minesweeper/Main.cs
@@ -8,6 +8,8 @@ public class Main : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    Texture2D _tileSpriteSheet;
+
     public Main()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -32,6 +34,7 @@ public class Main : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        _tileSpriteSheet = Content.Load<Texture2D>("minesweeper_spritesheet");
     }
 
     protected override void Update(GameTime gameTime)
@@ -49,6 +52,28 @@ public class Main : Game
         GraphicsDevice.Clear(new Color(243,243,243));
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+
+        int startingX = 100;
+        int startingY = 100;
+
+        int rows = 8;
+        int cols = 8;
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                _spriteBatch.Draw(
+                    _tileSpriteSheet,
+                    new Vector2(col*32 + startingX, row*32 + startingY),
+                    new Rectangle(0, 0, 32, 32),
+                    Color.White
+                );
+            }
+        }
+
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
